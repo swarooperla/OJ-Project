f720a05b9d2358c69068218015b47e992462407d
ERLA GNANA SWAROOP <erlagnanaswaroop05@gmail.com>
2025-06-27 00:25:42 +0530

    Whole CRUD completed

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-const API_URL = 'http://localhost:8000'
+const API_URL = import.meta.env.VITE_API_URL;
 
 function Login() {
     const [formData, setFromData] = useState({
