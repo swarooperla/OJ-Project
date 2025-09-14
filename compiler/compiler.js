58ba2e49011acb574682ea561f3599921ad9cda7
ERLA GNANA SWAROOP <erlagnanaswaroop05@gmail.com>
2025-09-14 16:44:10 +0530

    Fixed CORS in Compiler

--- compiler/compiler.js
+++ compiler/compiler.js
@@ -2,8 +2,25 @@ import express from "express";
 import cors from 'cors';
 import compilerRoutes from './routes/compilerRoutes.js'
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const allowedOrigins = [
+  "http://localhost:5173",                   
+  "https://oj-project-theta.vercel.app",
+  "https://bytecode.sbs"       
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // allow requests with no origin (like curl or Postman)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 
 app.get("/test", (req, res) => {
   res.send("Server is alive!");
